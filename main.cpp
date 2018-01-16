c4983c2baa30faadd722620017b8bb8e84017f10
jack <jackkaback@gmail.com>
2018-01-16 15:03:03 -0500

    Fliped every bit in a file

--- main.cpp
+++ main.cpp
@@ -6,12 +6,23 @@
 //EVIL BIT LEVEL HACKING
 using namespace std;
 
-void decrypt(vector<string> &lines){
-	string currLine;
-}
-
 void encrypt(vector<string> &lines){
 	string currLine;
+
+	for(int ii = 0; ii < lines.size(); ii++) {
+		currLine = lines[ii];
+
+		for(int jj = 0; jj < currLine.size(); jj++) {
+			bitset<8> bitset(currLine[jj]);
+
+			for (int k = 0; k < 8; k++){
+				bitset[k] = !bitset[k];
+			}
+
+			currLine[jj] = char(bitset.to_ullong());
+		}
+		lines[ii] = currLine;
+	}
 }
 
 void read(vector<string> &lines, string source){
@@ -47,32 +58,10 @@ int main() {
 	cin >> source;
 
 	read(lines, source);
+
+	encrypt(lines);
+
 	write(lines, source);
-
-	string test = lines[0];
-	bitset<8> testing(test[0]);
-
-	cout << testing << endl;
-	testing[0] = !testing[0];
-	test[0] = char(testing.to_ullong());
-
-	cout << test << endl;
-//	srand (time(NULL));
-//
-//	//8 bit int
-//	int8_t a = 89;
-//	cout << a << endl;
-//
-//	//convert to bit string
-//	bitset<8> x(a);
-//	cout << x << endl;
-//
-//	//invert a bit
-//	x[0] = !x[0];
-//	cout << x << endl;
-//
-//	//convert back to int
-//	a = (int)x.to_ulong();
 
 	return 0;
 }
